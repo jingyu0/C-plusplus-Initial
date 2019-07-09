fe8eb7c6df7e8771b47bb24faa26822f1428c47d
sunny <1404637683@qq.com>
2019-07-09 20:44:04 +0800

    C++11重点以及因式分解。

---
+++ 2019-07-09/2019-07-09/test.cpp
@@ -0,0 +1,75 @@
+//#include <iostream>
+//#include<stdlib.h>
+//using namespace std;
+//int main()
+//{
+//	int i , a;
+//	while (cin >> a)
+//	{
+//		cout << a <<" "<< "="<<" ";
+//		for (i = 2; i <= a; i++)
+//		{
+//			while (a != i)
+//			{
+//				if (a%i == 0)
+//				{
+//					cout << i <<" "<< "*"<<" ";
+//					a = a / i;
+//					i = 2;
+//				}
+//				else
+//					break;
+//			}
+//		}
+//		cout << a << endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//#include<math.h>
+//#include<stdlib.h>
+//#include<stdio.h>
+//#include<math.h>
+//#define EOF
+//int main()
+//{
+//	int n;
+//	while (scanf_s("%d", &n) != EOF(-1))
+//	{
+//		printf("%d =", n);
+//		int i;
+//		for (i = 2; i <= n; i++)
+//		{
+//			while (n != i)
+//			{
+//				if (n%i == 0)
+//				{
+//					printf(" %d *", i);
+//					n /= i;
+//				}
+//				else
+//					break;              //否则就陷入死循环了
+//			}
+//		}
+//		printf(" %d\n", n);
+//		system("pause");
+//	}
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int yy,mm,dd;
+//	while (cin >> yy)
+//	{
+//		cout << yy << "-" << 1 << "-" << 1 << endl;
+//		cout << yy << "-" << 7 << "-" << 4 << endl;
+//		cout << yy << "-" << 12 << "-" << 25 << endl;
+//	}
+//	//每组数据之间空行间隔
+//	cout << endl;
+//	return 0;
+//}
