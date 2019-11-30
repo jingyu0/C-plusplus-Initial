b7d0dcd4e13984002e3abe1456daddb54a80128b
jingyu0 <1404637683@qq.com>
2019-12-01 03:40:44 +0800

    提交代码小易的升级之路

--- upgrade.cpp
+++ upgrade.cpp
@@ -1,32 +1,34 @@
 //小易的升级之路
-#include <iostream>
-#include<stdlib.h>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
-int gcd(int m, int n)
-{
-	if (n == 0)
-		return m;
-	int temp = m%n;
-	m = n;
-	n = temp;
-	return gcd(m, n);
+
+//获取最大公约数
+int GetConventionNum(int b, int c) {
+	int ret = 0;
+	for (int i = 1; i <= b; ++i) {
+		if (b % i == 0 && c % i == 0)
+			ret = i;
+	}
+	return ret;
 }
 
-int main()
-{
-	int n, ans;
-	while (cin >> n >> ans)
-	{
-		while (n>0)
-		{
-	    	int cur;
-			cin >> cur;
-			ans = ans + ((ans<cur) ? gcd(cur, ans) : cur);
-			n--;
+int main() {
+	int n, a;
+	while (cin >> n >> a) {
+		vector<int> v(n);
+		for (size_t i = 0; i < v.size(); ++i) {
+			cin >> v[i];
 		}
-		cout << ans << endl;
+
+		for (size_t i = 0; i < v.size(); ++i) {
+			if (a > v[i])
+				a += v[i];
+			else
+				a += GetConventionNum(v[i], a);
+		}
+		cout << a << endl;
 	}
-	system("pause");
 	return 0;
 }
+
